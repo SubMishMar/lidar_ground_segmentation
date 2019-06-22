db30c166e7b8abd250e9ed5794d7713b46088ab4
SubMishMar <subodheenitr@gmail.com>
2019-06-22 03:36:43 -0500

    ground segmentation working

--- src/lidar_ground_segmentation_node.cpp
+++ src/lidar_ground_segmentation_node.cpp
@@ -1,32 +1,91 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
+#include "sensor_msgs/PointCloud2.h"
 
 #include <sstream>
 
+#include <pcl/common/common.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+#include <pcl_conversions/pcl_conversions.h>
+
+#include <pcl/ModelCoefficients.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+
+class groundSegmenter {
+private:
+    ros::NodeHandle nh;
+    ros::Subscriber cloud_sub;
+    ros::Publisher ground_plane_pub;
+
+public:
+    groundSegmenter() {
+        cloud_sub = nh.subscribe("/kitti/velo/pointcloud", 1,
+                &groundSegmenter::callback, this);
+        ground_plane_pub = nh.advertise<sensor_msgs::PointCloud2>
+                ("/kitti/velo/pointcloud_groundplane", 1);
+    }
+
+    void publishPlane(pcl::PointCloud<pcl::PointXYZ> cloud,
+                      std_msgs::Header header) {
+        sensor_msgs::PointCloud2 cloud_ros;
+        pcl::toROSMsg(cloud, cloud_ros);
+        cloud_ros.header.frame_id = header.frame_id;
+        cloud_ros.header.stamp = header.stamp;
+        ground_plane_pub.publish(cloud_ros);
+    }
+
+    void segmentCloud(pcl::PointCloud<pcl::PointXYZ> cloud_in,
+                      std_msgs::Header header) {
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>());
+        *cloud = cloud_in;
+
+        pcl::ModelCoefficients::Ptr coefficients
+                                (new pcl::ModelCoefficients);
+        pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+        /// Create the segmentation object
+        pcl::SACSegmentation<pcl::PointXYZ> seg;
+        /// Optional
+        seg.setOptimizeCoefficients (true);
+        /// Mandatory
+        seg.setModelType (pcl::SACMODEL_PLANE);
+        seg.setMethodType (pcl::SAC_RANSAC);
+        seg.setDistanceThreshold (0.05);
+        seg.setInputCloud (cloud);
+        seg.segment (*inliers, *coefficients);
+
+        std::vector<int> inlier_indices(inliers->indices);
+
+        if(inlier_indices.size() == 0) {
+            ROS_ERROR("No Plane Detected!");
+            ros::shutdown();
+        }
+
+//        ROS_INFO_STREAM("Model coefficients: "
+//                  << coefficients->values[0] << " "
+//                  << coefficients->values[1] << " "
+//                  << coefficients->values[2] << " "
+//                  << coefficients->values[3]);
+
+        pcl::PointCloud<pcl::PointXYZ>::Ptr
+                        plane(new pcl::PointCloud<pcl::PointXYZ>);
+        pcl::copyPointCloud(*cloud, inlier_indices, *plane);
+        publishPlane(*plane, header);
+    }
+
+    void callback(const sensor_msgs::PointCloud2::ConstPtr& cloud_msg) {
+        pcl::PointCloud<pcl::PointXYZ> cloud_in_pcl;
+        pcl::fromROSMsg(*cloud_msg, cloud_in_pcl);
+        segmentCloud(cloud_in_pcl, cloud_msg->header);
+    }
+};
+
 int main(int argc, char **argv) {
   ros::init(argc, argv, "lidar_ground_segmentation");
-  ros::NodeHandle n;
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
-  ros::Rate loop_rate(10);
-  int count = 0;
-  while (ros::ok())  {
-
-    std_msgs::String msg;
-
-    std::stringstream ss;
-    ss << "hello world " << count;
-    msg.data = ss.str();
-
-    ROS_INFO("%s", msg.data.c_str());
-
-    chatter_pub.publish(msg);
-
-    ros::spinOnce();
-
-    loop_rate.sleep();
-    ++count;
-  }
-
-
+  groundSegmenter gS;
+  ros::spin();
   return 0;
 }
